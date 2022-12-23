5990a2aa5abf486adf90a8dd0a0c5a6a3c61b303
Levente Simon <ls@leventesimon.com>
2022-12-23 10:49:20 +0100

    default k8s_config_path

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "os_user"                { type = string   }
 variable "cluster_hosts"          { type = map(any) }
-variable "k8s_config_path"        { type = string   }
+variable "k8s_config_path" {
+  type    = string
+  default = "/dev/null"
+}
 variable "cluster_name" {
   type    = string
   default = "rke-cluster"
